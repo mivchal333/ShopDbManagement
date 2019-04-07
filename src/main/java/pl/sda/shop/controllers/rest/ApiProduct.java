6f84c0cd09790175c45007b9ad82645242e9af99
Michal Krysiuk <mivchal@hotmail.com>
2019-04-07 13:29:34 +0200

    error when product doesn't exist

--- src/main/java/pl/sda/shop/controllers/rest/ApiProduct.java
+++ src/main/java/pl/sda/shop/controllers/rest/ApiProduct.java
@@ -1,12 +1,16 @@
 package pl.sda.shop.controllers.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.shop.domain.Product;
 import pl.sda.shop.service.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/product")
@@ -16,9 +20,16 @@ public class ApiProduct {
     ProductService service;
 
     @RequestMapping(value = "/all")
-    public List<Product> getAllProducts(){
-        System.out.println(service.listAllProducts());
+    public List<Product> getAllProducts() {
         return service.listAllProducts();
+    }
+
+    @RequestMapping("/{id}")
+    public ResponseEntity getProductById(@PathVariable Integer id) {
+        Optional<Product> productOpt = service.getProductById(id);
+        if (productOpt.isPresent()) {
+            return new ResponseEntity(productOpt.get(), HttpStatus.I_AM_A_TEAPOT);
+        } else return new ResponseEntity("Product not found", HttpStatus.BAD_REQUEST);
     }
 
 }
